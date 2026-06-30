9df131dece6e65ea3f71ab8e7411844aa1ab4f61
khoudia03 <cissemamy21@gmail.com>
2026-06-30 19:50:07 +0000

    feat: Enregistrement d'une nouvelle categorie

--- index.php
+++ index.php
@@ -36,6 +36,59 @@ $categories = [
     if (count($categorie["produits"]) == 0) {
          echo $categorie["nom"]."\n";
     }
- }
+ };
+
+
+
+
+  // 3: Enregister une nuovelle categorie
+
+$codeValide = true;
+    
+do { 
+        
+        $code = readline("saisir le code : ");
+        if (empty($code)) {
+            echo "le code est obligatoire \n";
+             $codeValide = false;
+        }else{
+            foreach ($categories as  $categorie) {
+               if (($categorie["code"]) === $code) {
+                $codeValide = false;
+                echo "Le code existe deja! Veuillez choisir un autre \n"; 
+              }
+            }  
+        }
+        
+} while (!$codeValide);
+
+    
+$nomValide = true;
+
+do { 
+
+        $nom = readline("saisir le nom : ");
+        if (empty($nom)) {
+            echo "le nom est obligatoire \n";
+             $nomValide= false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["nom"]) === $nom) {
+                $nomValide = false;
+                echo "Le nom existe deja! Veullez choisir un autre \n"; 
+               }
+            }  
+        }
+} while (!$nomValide);
+
+
+
+$categorie  =   [
+        "code" => $code,
+        "nom" => $nom,
+        "produits" => []
+    ];
+
+$categories[] = $categorie;
 
 ?>
